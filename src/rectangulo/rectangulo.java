9eb8a0e35709fd640da8e30929b59411aa4a9f82
Rodrigo Damian Martinez <t0tti-man@hotmail.com>
2023-11-19 18:53:46 -0300

    Agregue comentarios al proyecto

--- src/rectangulo/rectangulo.java
+++ src/rectangulo/rectangulo.java
@@ -14,21 +14,21 @@ public class rectangulo {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
-		double lado1, lado2, perimetro, area;
+		double lado1, lado2, perimetro, area; //DECLARO LAS VARIABLES
 		
-		System.out.print("Ingrese el primer lado de un rectangulo:");
+		System.out.print("Ingrese el primer lado de un rectangulo:"); //ESCRIBO EL LADO 1
 		lado1 = sc.nextDouble();
 		
-		System.out.print("Ingrese el segundo lado de un rectangulo:");
+		System.out.print("Ingrese el segundo lado de un rectangulo:");//ESCRIBO EL LADO 2
 		lado2 = sc.nextDouble();
 		
-		if (lado1 > 0 && lado2 > 0 && lado1 != lado2 ) {
+		if (lado1 > 0 && lado2 > 0 && lado1 != lado2 ) {//Digo que el lado 1 y el lado 2 deben ser mayor a cero, también digo que los lados no pueden ser iguales
 			perimetro = (lado1 * 2) + (lado2 * 2);		
 			System.out.println("El perimetro del rectangulo es: " + perimetro + "m");
 			
 			area = lado1 * lado2;	
 			System.out.println("El area del rectangulo es: " + area + "m²");
-		} else if (lado1 <= 0 || lado2 <=0) {
+		} else if (lado1 <= 0 || lado2 <=0) {//Digo que el lado 1 y el lado 2 no pueden ser menor a cero
 			System.out.println("El lado no puede ser cero");
 		} else {
 			System.out.println("Los lados no pueden ser iguales");
